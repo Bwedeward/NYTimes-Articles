65693685a2a995df0603c369d1de761080af8256
Brooke W <bwedeward@gmail.com>
2020-03-30 23:08:32 -0500

    created for loop append to html

--- assets/javascript/article.js
+++ assets/javascript/article.js
@@ -1,25 +1,37 @@
 //reference our URLs with API Key
-        //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
-    //Search specific word
-    //return certain amount of articles
+//https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+//Search specific word
+//return certain amount of articles
 //AJAX Call to "GET"
 //Then and function for response
 //Display response
 
+var article = "";
+
 $("#run-search").on("click", function() {
+  event.preventDefault();
+  console.log("hi");
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}&fq={filter}&MAGrWnieaVgAmwWdMRUtgxF9sZeXHeKJ"
+  var article = $(".form-control")
+    .val()
+    .trim();
+  console.log(article);
 
-$.ajax ({
+  var queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    article +
+    "&api-key=MAGrWnieaVgAmwWdMRUtgxF9sZeXHeKJ";
+
+  $.ajax({
     url: queryURL,
     method: "GET"
-});
+  }).then(function(res) {
+    console.log(res.response.docs[0]);
 
 
-.then(function(response){
-   console.log(response)
-    
-
-});
-
+    for (var i = 0; i < res.response.docs.length; i++){
+    var paragraph = $("<p>");
+    paragraph.text(res.response.docs[i].abstract);
+    $(".card-info").append(paragraph);}
+  })
 });
