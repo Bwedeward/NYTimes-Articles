9815acdc10a5f8da84ac5a148e3ca6b4dbd1d77a
Brooke W <bwedeward@gmail.com>
2020-03-30 21:10:38 -0500

    add query url and ajax call

--- assets/javascript/article.js
+++ assets/javascript/article.js
@@ -1,1 +1,19 @@
-test
+//reference our URLs with API Key
+        //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+    //Search specific word
+    //return certain amount of articles
+//AJAX Call to "GET"
+//Then and function for response
+//Display response
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=fq&MAGrWnieaVgAmwWdMRUtgxF9sZeXHeKJ"
+
+$.ajax ({
+    url: queryURL
+    method: "GET"
+});
+
+.then(function(response){
+   
+
+});
